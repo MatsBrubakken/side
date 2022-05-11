edb0a75693be2c715263210fd087c5ac73a0a363
Viktor Ericson <vcen21@student.aau.dk>
2022-05-12 01:36:12 +0200

    make it better

--- side/__init__.py
+++ side/__init__.py
@@ -7,8 +7,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"]="1234"
-#/app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://nbfptvkuaepcpb:93e79606b53715be5bd8b0c2c86f8c40662c490eecbb9b5323129860752b4b22@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d546vtlf0ekjsd"
-app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:admin@localhost/dummyside"
+app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://nbfptvkuaepcpb:93e79606b53715be5bd8b0c2c86f8c40662c490eecbb9b5323129860752b4b22@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d546vtlf0ekjsd"
+#/app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:admin@localhost/dummyside"
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
